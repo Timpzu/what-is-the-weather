eaecf00f58e45ac1a8c5374e3d997114ad4434c1
Timo <Timpzu@users.noreply.github.com>
2024-12-15 21:17:54 +0200

    Content update

--- js/functions.js
+++ js/functions.js
@@ -22,10 +22,10 @@ weatherQuery.addEventListener("change", async () => {
                     responseTertiary.innerHTML = `Teikäläine o vissii <span class="city-name">Helsingis</span>, isol kirkol.`;
                     break;
                 case "Pori":
-                    responseTertiary.innerHTML = `Ooks <span class="city-name">Poris?</span> Aiko hianoo.`;
+                    responseTertiary.innerHTML = `Ooks <span class="city-name">Poris</span>? Aiko hianoo.`;
                     break;
                 case "Nakkila":
-                    responseTertiary.innerHTML = `Ooks <span class="city-name">Nakkilas</span> vai, millai sää sin eksyit?`;
+                    responseTertiary.innerHTML = `Ooks <span class="city-name">Nakkilas</span> vai, millai sää sin oot eksyny?`;
                     break;
                 case "Rauma":
                     responseTertiary.innerHTML = `Ooks sää <span class="city-name">Raumal?!</span> Men ny menee siält!`;
@@ -43,30 +43,32 @@ weatherQuery.addEventListener("change", async () => {
                     const rainLastHour = weatherData.rain && weatherData.rain["1h"] ? weatherData.rain["1h"] : 0;
 
                     if (rainLastHour > 0) {
-                        responseMain.innerHTML = "Juu";
+                        responseMain.innerHTML = "Kyl vaa tiäks";
                         responseAdditional.innerHTML = `Sademäärä o ny <strong>${rainLastHour} mm/h</strong>.`;
                     } else {
                         responseMain.innerHTML = "Ei taid";
-                        responseAdditional.innerHTML = `Sademäärä o ny <strong>0 mm/h</strong>`;
+                        responseAdditional.innerHTML = `Sademäärä o ny <strong>0 mm/h</strong>.`;
                     }
                     break;
                 case "is-dark":
                     const unixUTC = Math.floor(Date.now() / 1000);
 
                     const sunriseDateObj = new Date(weatherData.sys.sunrise * 1000);
+                    sunriseDateObj.setHours(sunriseDateObj.getHours() + 2);
                     const sunriseTime = sunriseDateObj.toISOString().slice(11, 16);
 
                     const sunsetDateObj = new Date(weatherData.sys.sunset * 1000);
+                    sunsetDateObj.setHours(sunsetDateObj.getHours() + 2);
                     const sunsetTime = sunsetDateObj.toISOString().slice(11, 16);
 
                     if (unixUTC > weatherData.sys.sunrise && unixUTC < weatherData.sys.sunset) {
-                        responseMain.innerHTML = "Ei taid";
+                        responseMain.innerHTML = "Ei ol";
                         responseAdditional.innerHTML = `Aurinko nous tänää klo <strong>${sunriseTime}</strong> ja laskee tosa klo <strong>${sunsetTime}</strong>.`;
                     } else if (unixUTC > weatherData.sys.sunrise && unixUTC > weatherData.sys.sunset) {
-                        responseMain.innerHTML = "Juu";
+                        responseMain.innerHTML = "Juu o";
                         responseAdditional.innerHTML = `Aurinko nous tänää klo <strong>${sunriseTime}</strong> ja laski tosa klo <strong>${sunsetTime}</strong>.`;
                     } else {
-                        responseMain.innerHTML = "Juu";
+                        responseMain.innerHTML = "Juu o";
                         responseAdditional.innerHTML = `Aurinko nousee tänää klo <strong>${sunriseTime}</strong> ja laskee tosa klo <strong>${sunsetTime}</strong>.`;
                     }
                     break;
@@ -74,11 +76,11 @@ weatherQuery.addEventListener("change", async () => {
                     const temperatureCelsius = weatherData.main.temp - 273.15;
 
                     if (weatherData.main.temp < 283.15) {
-                        responseMain.innerHTML = "Juu";
-                        responseAdditional.innerHTML = `Ulkon o <strong>${temperatureCelsius.toFixed(0)} °C</strong>.`;
+                        responseMain.innerHTML = "Tarvii kyl";
+                        responseAdditional.innerHTML = `Ulkon o hei sit <strong>${temperatureCelsius.toFixed(0)} °C</strong>.`;
                     } else {
-                        responseMain.innerHTML = "Ei taid";
-                        responseAdditional.innerHTML = `Ulkon o <strong>${temperatureCelsius.toFixed(0)} °C</strong> lämmint.`;
+                        responseMain.innerHTML = "Ei kyl";
+                        responseAdditional.innerHTML = `Ulkon o hei <strong>${temperatureCelsius.toFixed(0)} °C</strong> lämmint.`;
                     }
                     break;
                 default:
@@ -109,7 +111,7 @@ weatherQuery.addEventListener("change", async () => {
 const positiveFeedback = document.getElementById("positive-feedback");
 positiveFeedback.addEventListener("click", () => {
     responseMain.innerHTML = "On kai!";
-    responseAdditional.innerHTML = `On kai se ny ko mää ole se tehny.`;
+    responseAdditional.innerHTML = `On kai se ny hyvä ko mää ole se tehny.`;
     responseTertiary.innerHTML = `...`;
     weatherQuery.selectedIndex = 0;
 });
